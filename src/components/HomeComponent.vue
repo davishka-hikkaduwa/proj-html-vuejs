1a13f26758039dedc43b71bda8313a7e4f5be688
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-10-05 14:27:22 +0200

    created home component

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="home">
+        <h1>BUILDING INSPIRING SPACES</h1>
+        <h4>We Build Inspiring Residential & Commercial Spaces</h4>
+        <div class="row">
+            <div class="col-6">
+                <button>EXPLORE RECENT WORK</button>
+            </div>
+            <div class="col-6">
+                <button>GET A QUOTE TODAY</button>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeComponent',
+    }
+</script>
+
+<style scoped lang="scss">
+    #home{
+        background-image: url("../assets/img/home-slider-62528242.jpg");
+        background-repeat:no-repeat;
+        background-position: fixed;
+        background-size: contain;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        h1{
+            color: #fff;
+        }
+        h4{
+            color: #f9d03f;
+        }
+    }
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,13 +1,15 @@
 <template>
     <main>
-        Main
+        <HomeComponent/>
     </main>
 </template>
 
 <script>
+import HomeComponent from '@/components/HomeComponent.vue';
     export default {
-        name: 'MainComponent',
-    }
+    name: "MainComponent",
+    components: { HomeComponent }
+}
 </script>
 
 <style scoped lang="scss">
