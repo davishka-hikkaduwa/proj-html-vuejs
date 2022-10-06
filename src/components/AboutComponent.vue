7c0f95331c5e073181bd1baf53a521bbe2a6df40
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-10-06 22:13:26 +0200

    completion AboutComponent

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -10,7 +10,14 @@
             <AboutCardComponent class="card-container"/>
         </div>
         <div class="feedback">
-
+            <div class="info-container">
+                <h5 class="custom-underline">Our Home Owners Say</h5>
+                <img src="@/assets/img/home-testimonial-113165296.jpg" alt="home-testimonial">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, architecto.
+                    Repellendus, aut. Vitae perferendis quaerat rerum odit quibusdam voluptatibus excepturi.
+                </p>
+                <h6>HARRY SMITH - NEW HOME OWNER</h6>
+            </div>
         </div>
     </div>
 </template>
@@ -28,20 +35,40 @@ import AboutCardComponent from "@/components/AboutCardComponent.vue";
         background-color: #f0ede6;
         padding: 30px 0;
         text-align: center;
-        .core-values{
-            .info-container{
-                padding: 40px 260px 0px;
+
+        .info-container{
+            padding: 40px 260px 0px;
                 
-                p{
-                    padding: 20px 40px 0px;
-                }
+            p{
+                padding: 20px 40px 0px;
             }
+        }
+
+        .core-values{
             .card-container{
                 padding: 20px 200px;
                 margin-bottom: 80px;
                 .row{
                     flex-wrap: nowrap;
                 } 
+            }     
+        }
+        .feedback{
+            padding-top: 80px;
+            padding-bottom: 80px;
+            color: #fff;
+            background-image: url('@/assets/img/home-parallax-144609983.jpg');
+            background-repeat:no-repeat;
+            background-size: cover;
+            
+            .info-container{
+                img{
+                    margin-top: 20px;
+                    border-radius: 50%;
+                }
+                h6{
+                    padding-top: 20px;
+                }
             }
         }
     }
