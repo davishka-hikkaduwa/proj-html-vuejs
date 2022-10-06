8b2c19e683d15339362094d546d1cf5c79c4ec7a
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-10-06 12:42:41 +0200

    added custom underline

--- src/components/ServicesComponent.vue
+++ src/components/ServicesComponent.vue
@@ -1,10 +1,14 @@
 <template>
     <div id="services">
-        <div class="upper">
+        <div class="heading-container">
             <h4>Do You Have A Construction Project We Can Help With?</h4>
         </div>
-        <div class="lower">
-
+        <div class="info-container">
+            <h5>Specialists in Modern Construction</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, architecto. Repellendus, aut. Vitae perferendis quaerat rerum odit quibusdam voluptatibus excepturi.</p>
+        </div>
+        <div class="card-container">
+            
         </div>
     </div>
 </template>
@@ -18,11 +22,31 @@
 
 <style scoped lang="scss">
     #services{
-        .upper{
-            text-align: center;
+        text-align: center;
+
+        .heading-container{
             padding: 30px 30px;
             background-color: #f9d03f;
             color: #1e1e1e;
+        }
+        .info-container{
+            padding: 80px 200px;
+            h5{
+                padding-bottom: 20px;
+                position: relative;
+            }
+            h5::before{
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 40%;
+                width: 140px;
+                height: 2px;
+                background-color: #f9d03f;
+            }
+            p{
+                padding-top: 20px;
+            }
         }
     }
 
