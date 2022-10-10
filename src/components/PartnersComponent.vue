328ae84232bf97f3031675c67c0f9bbf8411c9c7
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-10-10 14:32:43 +0200

    completed PartnersComponent

--- src/components/PartnersComponent.vue
+++ src/components/PartnersComponent.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="trusted-partners">
         <div class="info-container">
-            <SectionTitleComponent :title="sectionTitle"/>
+            <SectionTitleComponent :title="infoSectionTitle"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, architecto. Repellendus, aut. Vitae perferendis quaerat rerum odit quibusdam voluptatibus excepturi.</p>          
         </div>
         <div class="card-container row">
             <div class="card col" v-for="(card, index) in cards" :key="index">
-                <!-- <img :src="card.url" :alt="card.number"> -->
                 <img :src="`./assets/img/${card.url}`" :alt="index"/>
+            </div>
+        </div>
+        <div class="jumbotron row">
+            <div class="col-8 container">
+                <SectionTitleComponent :title="jumbotronSectionTitle"/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ipsam, assumenda perferendis fugit.</p>
+            </div>
+            <div class="col-4 container">
+                <button class="gold-btn">GET A FREE QUOTE</button>
+                <p>Only takes a few seconds!</p>
             </div>
         </div>
     </div>
@@ -19,7 +28,8 @@ export default {
     name: 'PartnersComponent',
     data(){
         return {
-            sectionTitle: 'Trusted Partners',
+            infoSectionTitle: 'Trusted Partners',
+            jumbotronSectionTitle: 'Building Inspiring Spaces',
             cards: [
                 {
                     url: 'home-logo11-219096700-320x202.png'
@@ -53,6 +63,38 @@ export default {
             .card{
                 // border: 1px solid black;
                 margin: 0 10px;
+            }
+        }
+        .jumbotron{
+            padding: 20px 150px;
+            margin-top: 70px;
+            background-image: url('@/assets/img/about-call-to-action-57862405.jpg');
+            background-repeat:no-repeat;
+            background-size: cover;
+            background-position-y: 80%;   
+            color: #fff;
+            height: 300px;
+            .container{
+                border: 1px solid #fff;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+            }   
+            .container.col-8{
+                text-align: left;               
+                align-items: flex-start;
+                padding: 0 20px;
+
+            }
+            .container.col-4{
+                padding: 0 40px;
+                button{
+                    margin: 10px 0;
+                }
+                p{
+                    font-size: 0.8rem;
+                    font-weight: bold;
+                }
             }
         }
     }
